44c067e385b87a42db3215423a16916bc9314339
Jarrod Folino <jarrod.folino@fundingcircle.com>
2023-12-09 16:49:18 +0000

    deploy

--- terraform/site/api_gateway.tf
+++ terraform/site/api_gateway.tf
@@ -1,5 +1,5 @@
 module "restapi" {
   source                      = "git::https://github.com/ExamonOrg/tf-modules.git//rest_api"
   api_name                    = "rest-api"
-  open_api_json_relative_path = "/Users/jarrod.folino/Dev/examon-proj/serverless-template/api_specification/openapi.json"
+  open_api_json_relative_path = "../../api_specification/openapi.json"
 }
