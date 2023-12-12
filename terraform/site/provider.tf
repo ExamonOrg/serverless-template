fbb11dde45f71b7e5b23fe5f4b7783970f13b65a
Jarrod Folino <jarrod.folino@fundingcircle.com>
2023-12-12 09:48:15 +0000

    Use tf state bucket

--- terraform/site/provider.tf
+++ terraform/site/provider.tf
@@ -5,4 +5,9 @@ terraform {
       version = ">= 3.0.0"
     }
   }
+  backend "s3" {
+    bucket = "examon-terraform-state"
+    key    = "serverless.template.terraform.tfstate"
+    region = "eu-west-1"
+  }
 }
